403394a9297f6b78c4987a1fb2e3a27a92cfe51d
= <sohail288@hotmail.com>
2016-04-22 22:28:19 -0700

    added search controller and template

--- app/controllers/site/controllers.py
+++ app/controllers/site/controllers.py
@@ -2,10 +2,19 @@
     This contains the controllers for the main site
 """
 
-from flask import (Blueprint, g, render_template, abort)
+from flask import (Blueprint,
+                   g,
+                   request,
+                   render_template,
+                   redirect,
+                   flash,
+                   url_for,
+                   abort)
+
 from . import main_routes
 from app.db import db_session
 from models.sheets import Sheetmusic
+
 
 
 
@@ -14,5 +23,12 @@ def index():
     return render_template('main.html')
 
 
-
+@main_routes.route('/results')
+def search_results():
+    q = request.args.get('q', None)
+    if q:
+        results = g.db.query(Sheetmusic).filter(Sheetmusic.title.like('%{}%'.format(q))).all()
+        return render_template('search_results.html', results=results, q = q)
+    flash('Need to search for something')
+    return redirect(url_for('.index'))
 
